1f091553a6e27ba8a2a9d07cbbf1ae87e1902ead
Daniel Golden <dangold@bergen.org>
2015-09-15 01:49:50 -0700

    finished hw1

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -15,42 +15,42 @@ class Foo {
     var wordB : String!
     
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]
+        wordB = words[1]
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q1 HERE]
-    
+//: The question marks after word[0] and word[1] are not necessary. The values of the array are already defined as being optional. Additionally, wordA and wordB are defined as being implicitly unwrapped, so if the goal was to unwrap them (it should be a ! not a ?), that is also unnecessary.
 
-    
+
+  
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
+    class func arePalindromes(words: [String]) -> Bool! {
         let reversedWords = words.map() {String($0.characters.reverse())}
         var numElements = words.count
-        
-        for let i = 0; i < numElements; i++ {
+      
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q2 HERE]
+//: You cannot modify a variable declared with let. You should use var instead. We should return either true or false, we should not be returning nil.
     
     
     
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
+    class func isAnagram(wordA: String, wordB: String) -> Bool? {
+        var countLetters = [Character : Int]()
         var lenA = wordA.characters.count
         var lenB = wordB.characters.count
-        
+      
         if lenA != lenB {
             return false
         }
@@ -81,11 +81,11 @@ class Foo {
             }
         }
         
-        return nil
+        return true
     }
 }
 
-//: [EXPLAIN YOUR ANSWER TO Q3 HERE]
+//: At the end we should return true, not nil. Additionally, the dictionary needs to be initialized, so you need to change the : to = and add parentheses at the end of it.
 
 
 //: **Do not** change anything below.
